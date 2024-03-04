4bb0596815c42f8ef9a8675caafa85a7db786cc3
Manish <mv998300@gmail.com>
2024-03-04 23:31:21 +0530

    added new files of EAS Build and sent to google for review

--- android/app/build/generated/source/buildConfig/debug/com/papercollection/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/debug/com/papercollection/BuildConfig.java
@@ -7,8 +7,8 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.papercollection";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.0";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "0.0.2";
   // Field from default config.
   public static final boolean IS_HERMES_ENABLED = true;
   // Field from default config.
